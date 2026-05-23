7dfa828a13f1660edd1a60405112b5c02b9bdc86
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-05-23 19:48:11 +0300

    simple font usage

--- dudewars/main.py
+++ dudewars/main.py
@@ -1,9 +1,22 @@
 import pygame 
 import sys 
 
+from pygame.color import THECOLORS
+
 pygame.init()
 
-screen = pygame.display.set_mode((1200, 800))
+size = (1200, 800)
+screen = pygame.display.set_mode(size)
+screen.fill(THECOLORS['orange'])
+
+font = pygame.font.SysFont('couriernew', 40)
+text = font.render(str('Hello there!'), True, THECOLORS['green'])
+screen.blit(text, (100, 50))
+
+# avaliable_fonts = pygame.font.get_fonts()
+# print(avaliable_fonts)
+
+
 r = pygame.Rect(0, 0, 100, 100)
 pygame.draw.rect(screen, (255, 0, 0), r, 0)
 
