137e9662506198b61bb625140ade854518c75a67
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-05-27 16:15:27 +0300

    Pygame tutorial first task - completed

--- dudewars/first_task.py
+++ dudewars/first_task.py
@@ -1,19 +1,18 @@
-import pygame
+import pygame 
 import sys
 
 from pygame.color import THECOLORS
 
 pygame.init()
 
-# setting canvas size 
 size = (1200, 800)
 screen = pygame.display.set_mode(size)
 screen.fill(THECOLORS['black'])
 
-# setting font size
-font = pygame.font.SysFont('coriernew', 40)
-
-# drawing 
+r = pygame.Rect(100, 100, 100, 100)
+pygame.draw.rect(screen, THECOLORS['green'], r, 0)
+pygame.draw.line(screen, THECOLORS['red'], (102, 100), (150, 20), 5)
+pygame.draw.line(screen, THECOLORS['red'], (198, 100), (150, 20), 5)
 
 while True:
     for event in pygame.event.get():
@@ -21,5 +20,6 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.flip()
+
 
 
