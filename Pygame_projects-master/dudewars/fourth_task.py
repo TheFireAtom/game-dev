dda1fe07d87b58d24b3c26d1d7fd1c0bd51ddc22
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-05-29 16:30:37 +0300

    Forth task: completed

--- Pygame_projects-master/dudewars/fourth_task.py
+++ Pygame_projects-master/dudewars/fourth_task.py
@@ -9,16 +9,18 @@ size = (1201, 801)
 screen = pygame.display.set_mode(size)
 screen.fill(THECOLORS['black'])
 
-cell_size = int(input("Enter cell size: "))
+font = pygame.font.SysFont(None, 80)
+
+# cell_size = int(input("Enter cell size: "))
+cell_size = 100
 temp_x = 0
 temp_y = 0
 
-def draw_one(cell_x, cell_y):
-    pygame.draw.lines(screen, THECOLORS['white'], False, 
-                      [(cell_x + 20, cell_y + 50), 
-                       (cell_x + 50, cell_y + 20), 
-                       (cell_x + 50, cell_y + 80)], 1)
-
+# def draw_one(cell_x, cell_y):
+#     pygame.draw.lines(screen, THECOLORS['white'], False, 
+#                       [(cell_x + 20, cell_y + 50), 
+#                        (cell_x + 50, cell_y + 20), 
+#                        (cell_x + 50, cell_y + 80)], 1)
 
 # old variant
 # for x in range(0, size[0] + cell_size, cell_size):
@@ -34,11 +36,14 @@ for pos in range(0, size[0] + cell_size, cell_size):
 
 
 for x in range(0, size[0] + cell_size, cell_size):  
-    for y in range(0, size[0] + cell_size, cell_size):
+    for y in range(0, size[1] + cell_size, cell_size):
         print(x, y)
-        if (x == 0):
-            draw_one(x, y)
-
+        number = x // cell_size + 1
+        text = font.render(str(number), False, THECOLORS['white'])
+        if (number < 10):
+            screen.blit(text, (x + 32, y + 25))
+        else:
+            screen.blit(text, (x + 20, y + 25))
 
 while True:
     for event in pygame.event.get():
