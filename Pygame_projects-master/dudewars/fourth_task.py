83b9b5ed195f329d2739e5784581b5c44b94457a
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-05-30 12:31:43 +0300

    almost done with the fourth task

--- Pygame_projects-master/dudewars/fourth_task.py
+++ Pygame_projects-master/dudewars/fourth_task.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 from pygame.color import THECOLORS
 
@@ -11,10 +12,46 @@ screen.fill(THECOLORS['black'])
 
 font = pygame.font.SysFont(None, 80)
 
+try: 
+    with open("data.txt", "r") as file:
+        print("Reading file...")
+        content = file.read()
+        print(content)
+       
+except FileNotFoundError:
+    with open("data.txt", "w") as file:
+        file.write("")
+    print("File created")
+
 # cell_size = int(input("Enter cell size: "))
-cell_size = 100
-temp_x = 0
-temp_y = 0
+cell_size = 100 # temporarly for debuging purposes 
+colors_list = ['black', 'green', 'blue', 'red', 'violet',
+               'purple', 'yellow', 'orange', 'brown', 'cyan']
+cnt = 0
+
+# some bullshit idk
+# def rand_nums(x, y):
+#     with open("data.txt", "w") as file:
+#         file.write("")
+#     rand_len = size[0] // cell_size
+#     # rand_list = []
+#     for rand_num in range(1, rand_len):
+#         rand_num = random.randint(1, rand_len)
+#         # rand_list.append()
+#         with open("data.txt", "a") as file:
+#             # print(rand_num)
+#             file.write(" ")
+#             file.write(str(rand_num))
+#     # print(rand_list) for debug
+#     # return rand_list
+#     with open("data.txt", "r") as file:
+#         print("Reading file...")
+#         content = file.read()
+#         numbers = [int(num) for num in content.split()]
+#         color_num = random.choice(numbers)
+#         #print(type(content))
+#         r = pygame.Rect(x, y, cell_size, cell_size)
+#         pygame.draw.rect(screen, THECOLORS[colors_list[color_num]], r, width=0)
 
 # def draw_one(cell_x, cell_y):
 #     pygame.draw.lines(screen, THECOLORS['white'], False, 
@@ -34,16 +71,47 @@ for pos in range(0, size[0] + cell_size, cell_size):
     pygame.draw.line(screen, THECOLORS['white'], (0, pos), (size[0], pos))
     # draw_one(pos, pos)
 
+# another bullshit, old design
+# for x in range(0, size[0] + cell_size, cell_size):  
+#     for y in range(0, size[1] + cell_size, cell_size):
+#         # print(x, y) for debugging
+#         number = x // cell_size + 1
+#         text = font.render(str(number), False, THECOLORS['white'])
+#         rand_nums(x, y)
 
-for x in range(0, size[0] + cell_size, cell_size):  
-    for y in range(0, size[1] + cell_size, cell_size):
-        print(x, y)
-        number = x // cell_size + 1
-        text = font.render(str(number), False, THECOLORS['white'])
-        if (number < 10):
+#         if (number == rand_nums and cnt < 1): 
+#             r = pygame.Rect(x, y, cell_size, cell_size)
+#             pygame.draw.rect(screen, THECOLORS[str(rand_nums)], r, width=0)
+#             cnt += 1
+
+#         if (number < 10):
+#             screen.blit(text, (x + 32, y + 25))
+#         else:
+#             screen.blit(text, (x + 20, y + 25))
+
+    with open("data.txt", "w") as file:
+        for _ in range(10):
+            rand_num = random.randint(1, size[0] // cell_size)
+            file.write((str(rand_num)) + " ")
+    
+with open("data.txt", "r") as file:
+    cells_to_paint = [int(num) for num in file.read().split()]
+
+cell_number = 1 # for tracking 
+
+for y in (0, size[1], cell_size):
+    for x in (0, size[0], cell_size):
+
+        if cell_number in cells_to_paint:
+            color = random.choice(colors_list)
+
+            r = pygame.Rect(x, y, cell_size, cell_size)
+            pygame.draw.rect(screen, THECOLORS[color], r)
+
+            text = font.render(str(cell_number), True, THECOLORS["white"])
             screen.blit(text, (x + 32, y + 25))
-        else:
-            screen.blit(text, (x + 20, y + 25))
+
+            cell_number += 1
 
 while True:
     for event in pygame.event.get():
