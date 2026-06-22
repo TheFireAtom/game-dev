02cfccf0e1f42dac60f2712437f0ea73899f1edc
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-06-22 21:37:24 +0300

    pygame quickstart

---
+++ Idk what/main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+# player_x = 100
+# player_y = 200
+#player_image = pygame.image.load("player.png").convert_alpha
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, "blue", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    pygame.display.update() # or flip
+
+    dt = clock.tick(60) / 1000
+
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
+
