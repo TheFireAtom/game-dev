b397b58e2edebb7a90cf4409098a1bcca04b27a2
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-07-18 13:38:24 +0300

    created pickable coins

--- Master_python_5_games_tutorial/My-games/Character-templater-test/test_again.py
+++ Master_python_5_games_tutorial/My-games/Character-templater-test/test_again.py
@@ -1,24 +1,67 @@
 import pygame
 from os.path import join
+from random import randint
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 720
+FRAME_WIDTH, FRAME_HEIGHT = 128, 128
 main_display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 clock = pygame.Clock()
 
+max_coins = 5
+coins = []
+coin_timer = pygame.time.get_ticks()
+coin_delay = 150
+
+all_sprites = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+
 def spritesheet_read(self, spritesheet, frames):
-    for x in range(int(spritesheet.get_width() / self.frame_width)):
-        frame = spritesheet.subsurface(x*self.frame_width, 0, self.frame_width, self.frame_height)
+    for x in range(int(spritesheet.get_width() / FRAME_WIDTH)):
+        frame = spritesheet.subsurface(x*FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT)
         frames.append(frame)
 
+def spawncoin():
+    global coin_timer
+    if len(coins) < max_coins:
+        if pygame.time.get_ticks() - coin_timer >= coin_delay:
+            for i in range(max_coins):
+                x = randint(10, WINDOW_WIDTH-10)
+                y = randint(10, WINDOW_HEIGHT-10)
+                coin = Coin(x, y)
+                coins.append(coin)
+                coin_group.add(coin)
+
+            coin_timer = pygame.time.get_ticks()
+        
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x, y, *groups):
+        super().__init__(*groups)
+
+        # self.frame_width = 128
+        # self.frame_height = 128
+
+        self.spritesheet_coin = pygame.image.load(join("My-games", "Character-templater-test", "assets", "coin_flip-Sheet.png")).convert_alpha()
+
+        self.spritesheet_coin_scaled = pygame.transform.scale_by(self.spritesheet_coin, 4)
+        self.image = self.spritesheet_coin_scaled.subsurface(0, 0, FRAME_WIDTH, FRAME_HEIGHT)
+        self.rect = self.image.get_frect()    
+
+        self.frames_coinflip = []    
+
+        spritesheet_read(self, self.spritesheet_coin_scaled, self.frames_coinflip)
+
+        self.rect.x = x
+        self.rect.y = y
+    
 class Player(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
 
         self.facing_right = True
 
-        self.spritesheet_walk_right = pygame.image.load(join("My-games", "Character-templater-test", "assets", "32x32_chibi_template_walk-Sheet.png")).convert_alpha()
+        self.spritesheet_walk_right = pygame.image.load(join("My-games", "Character-templater-test", "assets", "32x32_chibi_template_run_walk_new_anim-Sheet.png")).convert_alpha()
         self.spritesheet_idle = pygame.image.load(join("My-games", "Character-templater-test", "assets", "32x32_chibi_template_idle-Sheet.png")).convert_alpha()
         self.spritesheet_jump = pygame.image.load(join("My-games", "Character-templater-test", "assets", "32x32_chibi_template_jump-Sheet.png")).convert_alpha()
 
@@ -27,8 +70,8 @@ class Player(pygame.sprite.Sprite):
         self.spritesheet_idle_scaled = pygame.transform.scale_by(self.spritesheet_idle, 4)
         self.spritesheet_jump_scaled = pygame.transform.scale_by(self.spritesheet_jump, 4)
 
-        self.frame_width = 128
-        self.frame_height = 128
+        # self.frame_width = 128
+        # self.frame_height = 128
 
         self.frames_walk_right = []
         self.frames_walk_left = []
@@ -37,7 +80,7 @@ class Player(pygame.sprite.Sprite):
         self.frames_idle = []
         self.frames_jump = []
 
-        self.image = self.spritesheet_walk_right_scaled.subsurface(0, 0, self.frame_width, self.frame_height)
+        self.image = self.spritesheet_walk_right_scaled.subsurface(0, 0, FRAME_WIDTH, FRAME_HEIGHT)
         self.rect = self.image.get_frect()
 
         self.spritesheet_walk_left_scaled = pygame.transform.flip(self.spritesheet_walk_right_scaled, True, False)
@@ -64,11 +107,11 @@ class Player(pygame.sprite.Sprite):
             saved_position = self.rect.midleft
             self.frame_index += 1 
 
-            if self.player_direction.x > 0 and self.player_direction.y != 0:
-                self.image = self.frames_jump[self.frame_index % len(self.frames_jump)]
-            elif self.player_direction.x < 0 and self.player_direction.y != 0:
-                self.image = self.frames_jump[self.frame_index % len(self.frames_jump)]
-            elif self.player_direction.x > 0:
+            # if self.player_direction.x > 0 and self.player_direction.y != 0:
+            #     self.image = self.frames_jump[self.frame_index % len(self.frames_jump)]
+            # elif self.player_direction.x < 0 and self.player_direction.y != 0:
+            #     self.image = self.frames_jump[self.frame_index % len(self.frames_jump)]
+            if self.player_direction.x > 0:
                 self.image = self.frames_walk_right[self.frame_index % len(self.frames_walk_right)]
             elif self.player_direction.x < 0:
                 self.image = self.frames_walk_left[self.frame_index % len(self.frames_walk_left)]
@@ -100,8 +143,11 @@ class Player(pygame.sprite.Sprite):
         self.player_direction = self.player_direction.normalize() if self.player_direction else self.player_direction
         self.rect.center += self.player_direction * self.player_speed * dt
 
+    # def collision(self) {
 
-all_sprites = pygame.sprite.Group()
+    #     if pyga
+    # }
+
 player = Player(all_sprites)
 
 while running:
@@ -113,11 +159,16 @@ while running:
             running = False
 
     main_display.fill("white")
+
+    spawncoin()
     
     all_sprites.draw(main_display)
+    coin_group.draw(main_display)
 
     player.update()
     player.controls()
+
+    pygame.sprite.spritecollide(player, coin_group, True)
 
     pygame.display.update()
 
