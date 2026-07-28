a9b6f316d31290552b67d4e53a417ada86cfb09e
Ivan Kuleshov <Ivaner1234@yandex.ru>
2026-07-28 20:56:50 +0300

    Button size scale change

--- Master_python_5_games_tutorial/My-games/Character-templater-test/start_menu.py
+++ Master_python_5_games_tutorial/My-games/Character-templater-test/start_menu.py
@@ -9,8 +9,8 @@ def start_menu(main_display):
     timer = pygame.time.get_ticks()
     delay = 150
     frame_index = 0
-    spritesheet_start_button = pygame.image.load(join("My-games", "Character-templater-test", "assets", "Start_button-Sheet.png")).convert_alpha()
-    spritesheet_start_button_scaled = pygame.transform.scale_by(spritesheet_start_button, 4)
+    spritesheet_start_button = pygame.image.load(join("My-games", "Character-templater-test", "assets", "Play_button_square-Sheet.png")).convert_alpha()
+    spritesheet_start_button_scaled = pygame.transform.scale_by(spritesheet_start_button, 6)
     frames_start_button = []
     spritesheet_read(spritesheet_start_button_scaled, frames_start_button)
     #print(frames_start_button)
@@ -27,10 +27,10 @@ def start_menu(main_display):
 
         main_display.fill("white")
 
-        play_button = spritesheet_start_button_scaled.subsurface(0, 0, constants.FRAME_WIDTH, constants.FRAME_HEIGHT).get_frect()
+        play_button = spritesheet_start_button_scaled.subsurface(0, 0, constants.BUTTON_FRAME_WIDTH, constants.BUTTON_FRAME_HEIGHT).get_frect()
         quit_button = pygame.Rect(600, 600, 100, 100)
 
-        play_button.center = constants.WINDOW_WIDTH / 2 + 16, constants.WINDOW_HEIGHT / 2 - 32 # idk why I need to do those math operations here
+        play_button.center = constants.BUTTON_FRAME_WIDTH / 2, constants.BUTTON_FRAME_HEIGHT / 2 - 16 # idk why I need to do those math operations here
 
         main_display.blit(current_frame, play_button)
 
